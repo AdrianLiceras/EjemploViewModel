3ed4089fb5ce1b295129bc99c11911889d13cdab
Adrian <adrianliceras@gmail.com>
2022-03-02 12:22:40 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ejemploviewmodel/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.ejemploviewmodel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Toast
+import androidx.activity.viewModels
+import com.example.ejemploviewmodel.databinding.ActivityMainBinding
+
+class MainActivity  : AppCompatActivity(){
+
+    private lateinit var binding : ActivityMainBinding
+    private val viewModel : MainActivityViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initObserver()
+
+        binding.bDescargaUno.setOnClickListener {
+            viewModel.getPlanet()
+        }
+
+        binding.bDescargaTodos.setOnClickListener {
+            viewModel.getAllPlanets()
+        }
+    }
+    private fun initObserver() {
+        viewModel.isVisible.observe(this) { isVisible ->
+            if (isVisible) setVisible() else setGone()
+        }
+
+        viewModel.responseText.observe(this) { responseText ->
+            showToast(responseText)
+        }
+    }
+
+    private fun setVisible(){
+        binding.pbDownloading.visibility = View.VISIBLE
+    }
+    private fun setGone(){
+        binding.pbDownloading.visibility = View.GONE
+    }
+    private fun showToast(text : String) {
+        Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT).show()
+
+    }
+
+}
